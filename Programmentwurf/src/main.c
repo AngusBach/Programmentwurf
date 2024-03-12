e4ea075612fcfb175b486bea33d6720de241178c
AngusBach <156666508+AngusBach@users.noreply.github.com>
2024-03-12 13:35:12 +0100

    User input
    Definition of the four user inputs

--- Programmentwurf/src/main.c
+++ Programmentwurf/src/main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+#include "developer_group.h"
+#include "developer.h"
+
+
+int instruction;
 int main(void)
 {
     printf("\033[4;92m========================\n");
@@ -11,4 +16,25 @@ int main(void)
     printf("========================\033[0m\n");
 
     printf("Enter your selection: ");
+
+
+
+
+    scanf("%d",&instruction);
+
+    if(instruction==1){         //if instruction is 1, print developers
+
+    }
+    else if(instruction==2){    //if instruction is 2, print group logo
+
+    }
+    else if(instruction==3){    //if instruction is 3, print developers and group logo
+
+    }
+    else if(instruction==4){    //if instruction is 4, exit application
+
+    }
+    else{                       //else, nothing happens
+    
+    }
 }
