8dbcf29d086bf3c731471616de2909868fac5d95
faikhadut <156666285+faikhadut@users.noreply.github.com>
2024-03-14 13:48:08 +0100

    edited the runcmake.sh file
    we added make and rm commands for the automatisation

---
+++ Programmentwurf/build/CMakeFiles/libraries_developer_lib.dir/src/developer.c.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/libraries_developer_lib.dir/src/developer.c.o: \
+ /workspaces/Programmentwurf/Programmentwurf/src/developer.c \
+ /usr/include/stdc-predef.h \
+ /workspaces/Programmentwurf/Programmentwurf/include/developer.h
--- Programmentwurf/run_cmake.sh
+++ Programmentwurf/run_cmake.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 cmake  -S . -B build
+cd build
+make 
+./ libraries_ex_exec
+cd-build
+rm -rf build
