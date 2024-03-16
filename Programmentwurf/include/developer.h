5a52366e773ee1c94c529b8210b1b2fbd9de059f
faikhadut <156666285+faikhadut@users.noreply.github.com>
2024-03-16 21:52:31 +0100

    Comments
    comments for developer.h

--- Programmentwurf/include/developer.h
+++ Programmentwurf/include/developer.h
@@ -1,16 +1,16 @@
-#ifndef DEVELOPER_H_
+#ifndef DEVELOPER_H_ // Header guard to prevent multiple inclusion of this header file
 #define DEVELOPER_H_
 
-#define NAME_LENGTH (25U)
+#define NAME_LENGTH (25U) // Macro definition for the length of developer names
 
-typedef struct
+typedef struct // Defining a structure to hold developer information
 {
-    char name[NAME_LENGTH];
-    char aliase[NAME_LENGTH];
-}   developer;
+    char name[NAME_LENGTH]; // Name of the developer
+    char aliase[NAME_LENGTH]; // Alias of the developer
+}   developer; //specific name for our struct
 
 
-void print_developer(const developer *dev1, const developer *dev2);
+void print_developer(const developer *dev1, const developer *dev2); // Function for printing developer informations in main
 
 
 #endif
