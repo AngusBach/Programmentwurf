7872cc34d62f55fef0295bf3623461dfc7e6b6f3
faikhadut <156666285+faikhadut@users.noreply.github.com>
2024-03-16 23:45:40 +0100

    suprise
    we added the last spice

--- Programmentwurf/src/developer_group.c
+++ Programmentwurf/src/developer_group.c
@@ -1,5 +1,6 @@
 #include "developer_group.h" // Include the header file for developer group
 #include <stdio.h>
+#include <unistd.h>
 
 
 void print_group_logo(void) // Printing the group logo
@@ -21,7 +22,32 @@ void print_group_logo(void) // Printing the group logo
     printf("           ########           \n");
 
 
+  int blink_count = 5; // Number of times to blink
+    int blink_delay = 333333; // Delay in microseconds between blinks (approximately 3 times per second)
 
+    for (int j = 0; j < blink_count; j++) {
+        // Print the logo
+        printf("888b     d888                 888       888 d8b 888 888      d8b                                888               d888   \n");
+        printf("8888b   d8888                 888   o   888 Y8P 888 888      Y8P                                888              d8888   \n");
+        printf("88888b.d88888                 888  d8b  888     888 888                                         888                888   \n");
+        printf("888Y88888P888 888d888         888 d888b 888 888 888 888  888 888 88888b.   .d88b.      88888b.  888 .d8888b        888   \n");
+        printf("888 Y888P 888 888P\"           888d88888b888 888 888 888 .88P 888 888 \"88b d88P\"88b     888 \"88b 888 88K            888   \n");
+        printf("888  Y8P  888 888             88888P Y88888 888 888 888888K  888 888  888 888  888     888  888 888 \"Y8888b.       888   \n");
+        printf("888   \"   888 888     d8b     8888P   Y8888 888 888 888 \"88b 888 888  888 Y88b 888     888 d88P 888      X88       888   \n");
+        printf("888       888 888     Y8P     888P     Y888 888 888 888  888 888 888  888  \"Y88888     88888P\"  888  88888P'     8888888 \n");
+
+        fflush(stdout); // Flush the output buffer to ensure the content is printed immediately
+
+        usleep(blink_delay); // Pause execution for the blink delay
+
+        // Clear the screen
+        printf("\033[8A"); // Move cursor 8 lines up
+        printf("\033[0J"); // Clear from the cursor position to the end of the screen
+
+        fflush(stdout); // Flush the output buffer to ensure the content is cleared immediately
+
+        usleep(blink_delay); // Pause execution for the blink delay
+    }
 
 
 
