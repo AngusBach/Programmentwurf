5b7c15425ddd183cf5b87b6a94142a2abd597c8b
faikhadut <156666285+faikhadut@users.noreply.github.com>
2024-03-16 21:54:31 +0100

    Comments
    comments for developer_group.h

--- Programmentwurf/include/developer_group.h
+++ Programmentwurf/include/developer_group.h
@@ -1,6 +1,6 @@
-#ifndef DEVELOPER_GROUP_H_
+#ifndef DEVELOPER_GROUP_H_ // Header guard to prevent multiple inclusion of this header file
 #define DEVELOPER_GROUP_H_
 
-void print_group_logo(void);
+void print_group_logo(void); // Function prototype for printing group logo
 
 #endif
