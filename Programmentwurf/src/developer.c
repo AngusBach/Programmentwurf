8b0a0e3c781045566b9f74e6a55959368df94390
faikhadut <156666285+faikhadut@users.noreply.github.com>
2024-03-16 21:59:33 +0100

    Comments
    comments for developer_group.c

--- Programmentwurf/src/developer.c
+++ Programmentwurf/src/developer.c
@@ -1,6 +1,6 @@
-#include "developer.h"
+#include "developer.h" // Include the header file for developer information
 #include <stdio.h>
-
+// Printing our informations with our name and alias
 void print_developer(const developer *dev1, const developer *dev2) {
     printf("=====================\n");
     printf("Name:   %s\n", dev1->name);
